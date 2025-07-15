ab50642ed76d28b855e244ba93b578d8e069a043
Thabang <phoshokothabang1@gmail.com>
2025-07-15 19:56:05 +0200

    feat: get an input from the user

--- Week 7/string.py
+++ Week 7/string.py
@@ -64,3 +64,12 @@ elif num == 0:
     print("Zero")
 else: 
     print("Negative number")
+    
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+if num1 > num2:
+    print(f"{num1} is greater than {num2}") 
+elif num1 == num2:
+    print(f"{num1} is equal to {num2}")
+else:   
+    print(f"{num1} is less than {num2}")
