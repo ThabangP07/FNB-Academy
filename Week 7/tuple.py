c1bf6a5a970e593faccfd447bd66f6f0510c71ee
Thabang <phoshokothabang1@gmail.com>
2025-07-16 21:49:17 +0200

    feat: tuple operations

--- Week 7/tuple.py
+++ Week 7/tuple.py
@@ -8,3 +8,9 @@ print(my_tuple)
 # Accessing elements in a tuple
 print(my_tuple[0])  # Output: 1
 print(my_tuple[-1])  # Output: (5)
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+combined_tuple = tuple1 + tuple2  # Concatenation
+print(combined_tuple)  # Output: (1, 2, 3, 4, 5, 6)
+
