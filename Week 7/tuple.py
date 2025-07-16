e70e286334322d53eeb8a2609df83c04b6445717
Thabang <phoshokothabang1@gmail.com>
2025-07-16 21:45:13 +0200

    feat: access elements of  a tuple

---
+++ Week 7/tuple.py
@@ -0,0 +1,10 @@
+# ----------------  TUPLES ----------------
+
+# Tuples are immutable sequences, typically used to store collections of heterogeneous data.
+
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)  
+
+# Accessing elements in a tuple
+print(my_tuple[0])  # Output: 1
+print(my_tuple[-1])  # Output: (5)
