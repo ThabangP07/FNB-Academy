22417235c7b348d336c849b3306edad467f504e0
Thabang <phoshokothabang1@gmail.com>
2025-07-16 22:05:29 +0200

    feat: accessing elements of a dictionary

---
+++ Week 7/dict.py
@@ -0,0 +1,14 @@
+# ------------------ DICTIONARY ------------------
+
+my_dict = {'apple': 1, 'banana': 2, 'cherry': 3}
+
+# Accessing elements in a dictionary
+print(my_dict['apple'])  # Output: 1
+
+# Adding a new key-value pair
+my_dict['orange'] = 4
+print(my_dict)  # Output: {'apple': 1, 'banana': 2, 'cherry': 3, 'orange': 4}
+
+# Modifying an existing key-value pair
+my_dict['banana'] = 5   
+print(my_dict)  # Output: {'apple': 1, 'banana': 5, 'cherry': 3, 'orange': 4}
