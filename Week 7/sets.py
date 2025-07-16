76b1bc3c2f83f9ff67427dfb47d565753659e6cb
Thabang <phoshokothabang1@gmail.com>
2025-07-16 21:54:04 +0200

    feat: adding and removing elements in a set

---
+++ Week 7/sets.py
@@ -0,0 +1,13 @@
+# ---------- SETS ----------
+
+# Sets are unordered collections of unique elements.
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)  # Output: {1, 2, 3, 4, 5}    
+
+# Adding elements to a set
+my_set.add(6)  # Add a single element
+
+# Removing elements from a set
+my_set.remove(2)  # Remove a specific element       
+print(my_set)  # Output: {1, 3, 4, 5, 6}
