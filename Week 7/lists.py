bd07fd6e3f61648b1da1ecb15d2e2a80cc91133a
Thabang <phoshokothabang1@gmail.com>
2025-07-16 21:37:47 +0200

    feat: add or remove items from the list

--- Week 7/lists.py
+++ Week 7/lists.py
@@ -6,3 +6,14 @@ print(fruits[0])
 
 fruits[1] = "blueberry"  # Change banana to blueberry
 print(fruits)
+
+# list methods
+
+fruits.append("orange")  # Add orange to the end of the list
+print(fruits)
+
+fruits.insert(1, "kiwi")  # Insert kiwi at index 1
+print(fruits)
+
+fruits.remove("cherry")  # Remove cherry from the list
+print(fruits)
