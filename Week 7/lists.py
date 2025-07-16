050f229e0ae20e4310ef47d092443dc9e46b1e8d
Thabang <phoshokothabang1@gmail.com>
2025-07-16 21:33:14 +0200

    feat: list manipulation

---
+++ Week 7/lists.py
@@ -0,0 +1,8 @@
+# ----------lISTS----------
+# This is a simple list of fruits
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+fruits[1] = "blueberry"  # Change banana to blueberry
+print(fruits)
