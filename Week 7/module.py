f0e5948d5cd12b23f69fbb3dcc364fd65640b55f
Thabang <phoshokothabang1@gmail.com>
2025-07-18 20:01:51 +0200

    feat: import functions from math operations file

---
+++ Week 7/module.py
@@ -0,0 +1,7 @@
+import math_operations
+
+print(math_operations.add(5, 10))  # Example usage of the add function
+print(math_operations.subtract(10, 5))  # Example usage of the subtract function
+print(math_operations.multiply(4, 3))  # Example usage of the multiply function
+print(math_operations.divide(20, 4))  # Example usage of the divide function
+print(math_operations.divide(10, 0))  # Example usage of division by zero
