0172e86a97a67d7f6ac7e62385b66a7c9e0dc9c2
Thabang <phoshokothabang1@gmail.com>
2025-07-18 20:22:20 +0200

    feat: import built in python modules

---
+++ Week 7/builtin_modules.py
@@ -0,0 +1,15 @@
+import math
+import random
+import datetime
+import os
+import sys
+
+print(math.pi)
+
+print(random.randint(1, 10))  # Random number between 1 and 10
+
+print(datetime.datetime.now()) # Prints the current date and time
+
+print(os.getcwd()) # Prints the current working directory
+
+print(sys.version) # Prints out the current version of python
